61e1b2e2ddc9c649f7a85166a252173d3c484e34
Greed <suby0475@gmail.com>
2024-04-18 15:52:19 +0900

    ✨ feat(network): network 통신 error Handling

---
+++ TaxiParty/Network/Model/ValidationEmailModel.swift
@@ -0,0 +1,12 @@
+//
+//  ValidationEmailModel.swift
+//  TaxiParty
+//
+//  Created by Greed on 4/16/24.
+//
+
+import Foundation
+
+struct ValidationEmailModel: Decodable {
+    let message: String
+}
--- TaxiParty/Network/NetworkManager.swift
+++ TaxiParty/Network/NetworkManager.swift
@@ -13,13 +13,17 @@ enum NetworkError: Error {
     case invalidURL
     case invalidStatusCode
     case failedRequest
+    case cantUseIt
+    case invalidSesacKey
+    case overCalling
+    case invalidRequest
 }
 
 final class NetworkManager {
     static let shared = NetworkManager()
     
     func callRequest<T>(type: T.Type, router: TargetType) -> Single<Result<T, NetworkError>> where T: Decodable {
-        return Single.create { single in
+        return Single.create { single -> Disposable in
             
             do {
                 let urlRequest = try router.asURLRequest()
@@ -29,8 +33,25 @@ final class NetworkManager {
                         switch response.result {
                         case .success(let success):
                             single(.success(.success(success)))
+                            print(success)
                         case .failure(let error):
-                            single(.failure(error))
+                            print(error)
+                            let networkError: NetworkError
+                            switch error.responseCode {
+                            case 409:
+                                networkError = .cantUseIt
+                            case 420:
+                                networkError = .invalidSesacKey
+                            case 429:
+                                networkError = .overCalling
+                            case 444:
+                                networkError = .invalidURL
+                            case 500:
+                                networkError = .invalidRequest
+                            default:
+                                networkError = .failedRequest
+                            }
+                            single(.success(.failure(networkError)))
                         }
                     }
             } catch {
