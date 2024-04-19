640b2d7843f7281b063ff1106fac2a1ef698f084
Greed <suby0475@gmail.com>
2024-04-19 15:02:54 +0900

    ✨ feat(Token): Property Wrapper를 이용한 Token 관리 매니저 생성

---
+++ TaxiParty/Util/Token/TokenDefaults.swift
@@ -0,0 +1,38 @@
+//
+//  Token.swift
+//  TaxiParty
+//
+//  Created by Greed on 4/18/24.
+//
+
+import Foundation
+
+@propertyWrapper
+struct TokenDefaults<T> {
+    let key: String
+    let defaultValue: T
+    
+    var wrappedValue: T {
+        get {
+            UserDefaults.standard.object(forKey: key) as? T ?? defaultValue
+        }
+        set {
+            UserDefaults.standard.set(newValue, forKey: key)
+        }
+    }
+    
+}
+
+enum TokenManager {
+    enum Key: String {
+        case accessToken
+        case refreshToken
+    }
+    
+    @TokenDefaults(key: Key.accessToken.rawValue, defaultValue: "액세스 토큰 없음")
+    static var accessToken
+    
+    @TokenDefaults(key: Key.refreshToken.rawValue, defaultValue: "리프레시 토큰 없음")
+    static var refreshToken
+    
+}
