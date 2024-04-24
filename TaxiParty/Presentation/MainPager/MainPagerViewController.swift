2cb6c7df0125c3b967ddd2ccbad020e69f64db42
Greed <suby0475@gmail.com>
2024-04-24 17:22:06 +0900

    📱 design(tabBar): 탭바 디자인 변경 및 컬러 변경

--- TaxiParty/Presentation/MainPager/MainPagerViewController.swift
+++ TaxiParty/Presentation/MainPager/MainPagerViewController.swift
@@ -33,7 +33,7 @@ struct tabSwiftUIView: View {
     enum TabViews: Int, CaseIterable {
         case searchPostView
         case nearMapView
-        case postPartyView
+        case addPostView
         case chatListView
         case settingView
         
@@ -43,8 +43,8 @@ struct tabSwiftUIView: View {
                 return AnyView(SearchPostRepresentableView())
             case .nearMapView:
                 return AnyView(NearMapRepresentableView())
-            case .postPartyView:
-                return AnyView(SettingRepresentableView())
+            case .addPostView:
+                return AnyView(AddPostRepresentableView())
             case .chatListView:
                 return AnyView(ChatListRepresentableView())
             case .settingView:
@@ -58,7 +58,7 @@ struct tabSwiftUIView: View {
                 return "house"
             case .nearMapView:
                 return "map"
-            case .postPartyView:
+            case .addPostView:
                 return "plus.app"
             case .chatListView:
                 return "bubble"
@@ -72,7 +72,6 @@ struct tabSwiftUIView: View {
     let timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
     
     var body: some View {
-#if swift(>=5.9)
         if #available(iOS 17.0, *) {
             tabbars()
                 .onReceive(timer) { input in
@@ -81,9 +80,6 @@ struct tabSwiftUIView: View {
         } else {
             tabbars()
         }
-#else
-        tabbars()
-#endif
     }
     
     func tabbars() -> some View {
@@ -92,16 +88,15 @@ struct tabSwiftUIView: View {
             VStack() {
                 AnimatedTabBar(selectedIndex: $selectedIndex,
                                views: TabViews.allCases.map { wiggleButtonAt($0.rawValue, name: $0.tabImage)})
-                .cornerRadius(16)
+                .cornerRadius(24)
                 .selectedColor(.pointPurple)
-                .unselectedColor(.gray.opacity(0.7))
+                .unselectedColor(.gray.opacity(0.3))
                 .ballColor(.pointPurple)
-                .verticalPadding(20)
-                .barColor(.pointPurple.opacity(0.1))
-                Spacer().frame(height: 10)
+                .verticalPadding(30)
+                .barColor(.tabbar)
             }
             .frame(maxWidth: .infinity)
-            .padding(8)
+            .padding(.horizontal, 2)
         }
         .ignoresSafeArea(.all)
     }
@@ -111,63 +106,3 @@ struct tabSwiftUIView: View {
             .scaleEffect(1.3)
     }
 }
-
-//import Tabman
-//import Pageboy
-//
-//final class MainPagerViewController: TabmanViewController {
-//
-//    private var viewControllers: Array<UIViewController> = []
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        view.backgroundColor = .white
-//
-//    }
-//
-//    override func viewWillAppear(_ animated: Bool) {
-//        super.viewWillAppear(animated)
-//        setTabman()
-//    }
-//
-//    private func setTabman() {
-//        hideBackButton()
-//        [SearchPostViewController(), NearMapViewController(), SettingViewController()].forEach { tabView in
-//            viewControllers.append(tabView)
-//        }
-//
-//        self.dataSource = self
-//        self.isScrollEnabled = true
-//
-//        let bar = TMBar.ButtonBar()
-//        bar.layout.transitionStyle = .snap
-//        bar.indicator.weight = .custom(value: 1)
-//        bar.indicator.tintColor = .pointPurple
-//        bar.layout.alignment = .centerDistributed
-//        bar.layout.interButtonSpacing = 60
-//
-//        addBar(bar, dataSource: self, at: .navigationItem(item: self.navigationItem))
-//    }
-//
-//}
-//
-//extension MainPagerViewController: PageboyViewControllerDataSource, TMBarDataSource {
-//    func numberOfViewControllers(in pageboyViewController: Pageboy.PageboyViewController) -> Int {
-//        return viewControllers.count
-//    }
-//
-//    func viewController(for pageboyViewController: Pageboy.PageboyViewController, at index: Pageboy.PageboyViewController.PageIndex) -> UIViewController? {
-//        return viewControllers[index]
-//    }
-//
-//    func defaultPage(for pageboyViewController: Pageboy.PageboyViewController) -> Pageboy.PageboyViewController.Page? {
-//        return nil
-//    }
-//
-//    func barItem(for bar: any Tabman.TMBar, at index: Int) -> any Tabman.TMBarItemable {
-//        let item = TMBarItem(title: "")
-//        let title = ["검색", "근처팟찾기", "마이페이지"]
-//        item.title = title[index]
-//        return item
-//    }
-//}
